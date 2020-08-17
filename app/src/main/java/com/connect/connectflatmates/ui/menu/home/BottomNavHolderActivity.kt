246a7ae49e2c75246e3d65af0253678a28ec9220
Maksym <bussarpl@gmail.com>
2020-08-17 23:10:04 +0200

    deleted toolbar arrow back

--- app/src/main/java/com/connect/connectflatmates/ui/menu/home/BottomNavHolderActivity.kt
+++ app/src/main/java/com/connect/connectflatmates/ui/menu/home/BottomNavHolderActivity.kt
@@ -2,12 +2,15 @@ package com.connect.connectflatmates.ui.menu.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.connect.connectflatmates.R
 import kotlinx.android.synthetic.main.activity_bottom_nav_holder.*
+import kotlinx.android.synthetic.main.activity_navigation_drawer_holding.*
 
 class BottomNavHolderActivity : AppCompatActivity() {
 
@@ -19,8 +22,24 @@ class BottomNavHolderActivity : AppCompatActivity() {
 
         navController = Navigation.findNavController(this, R.id.homeActivities_navHost)
 
-        NavigationUI.setupWithNavController(topAppBar, navController)
+        setupToolbar()
 
         homeActivities_bottomNav.setupWithNavController(navController)
+    }
+
+
+    private fun setupToolbar() {
+        val toolbarLayout: Toolbar = topAppBar
+        setSupportActionBar(toolbarLayout)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.myActivities,
+                R.id.addHomeActivity,
+                R.id.unsingedActivities,
+                R.id.findFlatmates
+            )
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 }
